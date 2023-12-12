7baa4d83ee17a1669d99b2a2b2fe825de34be4bc
Alstudd <alstonsoares17@gmail.com>
2023-12-12 19:14:14 +0530

    user login and logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Main, { mainLoader } from "./layouts/Main";
 
 // Routes
-import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 
 // Actions
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
         path: "/", // index: true,
         element: <Dashboard />,
         loader: dashboardLoader,
+        action: dashboardAction,
         errorElement: <Error />,
       },
       {
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,11 +5,29 @@ import { fetchData } from "../helpers"
 // components
 import Hero from "../components/Hero"
 
+// react-toastify
+import { toast } from "react-toastify"
+
 // loader
 export function dashboardLoader() {
     const userName = fetchData("userName") // const userName = "John"
     return { userName } // return { userName: "John" }
 }
+
+// actions
+export async function dashboardAction({ request }) {
+    const data = await request.formData();
+    // console.log({ data, request })
+    // const userName = data.get("userName")
+    const formdata = Object.fromEntries(data)
+    try {
+        localStorage.setItem("userName", JSON.stringify(formdata.userName))
+        return toast.success(`Welcome ${formdata.userName}!`)
+    } catch (e) {
+        throw new Error("Error creating user!")
+    }
+}
+
 
 function Dashboard() {
     const { userName } = useLoaderData() // const { userName } = { userName: "John" }
